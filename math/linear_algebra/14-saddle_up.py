326841eec0d2ea167de4912ed7b8f70e17a09e95
Relebohile Pheko <145283035+GLADYS-ARCH@users.noreply.github.com>
2025-05-24 00:01:29 +0200

    saddle up

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -2,5 +2,15 @@
 import numpy as np
 
 def np_matmul(mat1, mat2):
-    return np.matmul(mat1, mat2)
+    """
+    Performs matrix multiplication of two numpy ndarrays.
 
+    Args:
+        mat1 (numpy.ndarray): The first matrix.
+        mat2 (numpy.ndarray): The second matrix.
+
+    Returns:
+        numpy.ndarray: The product of mat1 and mat2.
+    """
+    # Use numpy's matmul function to multiply the matrices
+    return np.matmul(mat1, mat2)
