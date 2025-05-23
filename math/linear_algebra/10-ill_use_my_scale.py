42883ac6cd10144c69c320118e181365d43e5528
Relebohile Pheko <145283035+GLADYS-ARCH@users.noreply.github.com>
2025-05-23 23:46:01 +0200

    matrix shape

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
-def np_shape(matrix):
-    return matrix.shape
+"""
+Module that defines a function to get the shape of a NumPy matrix.
+"""
 
+def np_shape(matrix):
+    """
+    Returns the shape of a NumPy matrix.
+
+    Args:
+        matrix (numpy.ndarray): The input matrix.
+
+    Returns:
+        tuple: A tuple representing the shape of the matrix.
+    """
+    return matrix.shape
