bf033585e22fdc33f305ce147673b3035f64ea58
Relebohile Pheko <145283035+GLADYS-ARCH@users.noreply.github.com>
2025-05-24 01:18:09 +0200

    last one

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,54 +1,51 @@
+#!/usr/bin/env python3
+
+def shape(mat):
+    """Return the shape of a nested list (matrix)."""
+    if not isinstance(mat, list):
+        return ()
+    if len(mat) == 0:
+        return (0,)
+    return (len(mat),) + shape(mat[0])
+
+def check_shapes_compatible(shape1, shape2, axis):
+    """Check if two shapes are compatible for concatenation along axis."""
+    if len(shape1) != len(shape2):
+        return False
+    for i in range(len(shape1)):
+        if i == axis:
+            continue
+        if shape1[i] != shape2[i]:
+            return False
+    return True
+
+def concat_axis(mat1, mat2, axis):
+    """Concatenate two matrices along the given axis recursively."""
+    if axis == 0:
+        # Concatenate top level lists
+        return mat1 + mat2
+    else:
+        # axis > 0, recurse into sublists
+        if len(mat1) != len(mat2):
+            # shapes don't match along other dimensions, fail
+            return None
+        result = []
+        for i in range(len(mat1)):
+            concatenated = concat_axis(mat1[i], mat2[i], axis - 1)
+            if concatenated is None:
+                return None
+            result.append(concatenated)
+        return result
+
 def cat_matrices(mat1, mat2, axis=0):
     """
-    Concatenate two matrices (nested lists) along a specified axis.
-
-    Args:
-        mat1 (list): First matrix (nested lists).
-        mat2 (list): Second matrix (nested lists).
-        axis (int): Axis along which to concatenate.
-
-    Returns:
-        list: New concatenated matrix or None if not possible.
+    Concatenate two matrices along a specific axis.
+    Return new matrix or None if shapes incompatible.
     """
-
-    def shape(matrix):
-        """Get shape of nested lists."""
-        if not isinstance(matrix, list):
-            return ()
-        if len(matrix) == 0:
-            return (0,)
-        return (len(matrix),) + shape(matrix[0])
-
-    def check_shapes_compatible(s1, s2, axis):
-        """Check if shapes are compatible except on concatenation axis."""
-        if len(s1) != len(s2):
-            # Shapes must have same number of dimensions
-            return False
-        for i, (d1, d2) in enumerate(zip(s1, s2)):
-            if i == axis:
-                continue
-            if d1 != d2:
-                return False
-        return True
-
-    def concat(m1, m2, axis):
-        """Recursively concatenate along axis."""
-        if axis == 0:
-            # Concatenate lists at this level
-            return m1 + m2
-        else:
-            # Recurse on sublists
-            return [concat(sub1, sub2, axis - 1) for sub1, sub2 in zip(m1, m2)]
-
     shape1 = shape(mat1)
     shape2 = shape(mat2)
-
-    if axis < 0 or axis >= len(shape1):
-        # Axis out of bounds
-        return None
 
     if not check_shapes_compatible(shape1, shape2, axis):
         return None
 
-    # Perform concat
-    return concat(mat1, mat2, axis)
+    return concat_axis(mat1, mat2, axis)
