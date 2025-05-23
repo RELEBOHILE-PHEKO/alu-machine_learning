ee3e61a40792434fb7a02df5fd6710ffc15c5536
Relebohile Pheko <145283035+GLADYS-ARCH@users.noreply.github.com>
2025-05-23 22:55:44 +0200

    adds two arrays element-wise

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
-def add_arrays(arr1, arr2):
-    if len(arr1) != len(arr2):
-        return None
-    return [a + b for a, b in zip(arr1, arr2)]
+"""
+Module that contains a function to add two arrays element-wise.
+"""
 
+
+def add_arrays(arr1, arr2):
+    """
+    Adds two arrays (lists) element-wise.
+
+    Args:
+        arr1 (list): The first list of integers/floats.
+        arr2 (list): The second list of integers/floats.
+
+    Returns:
+        list or None: A list containing the sum of corresponding elements,
+                      or None if the input lists are not the same length.
+    """
+    if len(arr1) != len(arr2):
+        return None  # Return None if array lengths don't match
+
+    return [a + b for a, b in zip(arr1, arr2)]  # Element-wise addition
