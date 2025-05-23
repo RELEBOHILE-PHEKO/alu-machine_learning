1657b74f890cd59967b4f514095851c2b7d5cde8
Relebohile Pheko <145283035+GLADYS-ARCH@users.noreply.github.com>
2025-05-23 23:53:18 +0200

    transpose the matrix

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+"""
+Module for transposing a NumPy matrix.
+"""
+
 import numpy as np
 
-def np_transpose(matrix):
-    return np.transpose(matrix)
 
+def np_transpose(matrix):
+    """
+    Transposes a NumPy matrix.
+
+    Args:
+        matrix (numpy.ndarray): The matrix to transpose.
+
+    Returns:
+        numpy.ndarray: The transposed matrix.
+    """
+    return np.transpose(matrix)
