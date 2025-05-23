9290247063f1c0f835e090f8fe4ff5014cc4d9a4
Relebohile Pheko <145283035+GLADYS-ARCH@users.noreply.github.com>
2025-05-24 00:21:34 +0200

    Bracing the elements

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
-def np_elementwise(mat1, mat2):
-    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
 
+def np_elementwise(mat1, mat2):
+    """
+    Performs element-wise addition, subtraction, multiplication, and division
+    on two matrices.
+
+    Args:
+        mat1 (numpy.ndarray): The first matrix.
+        mat2 (numpy.ndarray): The second matrix.
+
+    Returns:
+        tuple: A tuple containing four numpy.ndarrays representing the element-wise
+               sum, difference, product, and quotient of mat1 and mat2, respectively.
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
