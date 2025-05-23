66065e889ed77238b8eefe1fa5f16bf80d686904
Relebohile Pheko <145283035+GLADYS-ARCH@users.noreply.github.com>
2025-05-23 22:50:21 +0200

    returns the transpose of a 2D matrix

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
-def matrix_transpose(matrix):
-    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+"""
+Module that contains a function for transposing matrices.
+"""
 
+
+def matrix_transpose(matrix):
+    """
+    Transposes a 2D matrix.
+
+    Args:
+        matrix (list of lists): A 2D list representing the matrix to transpose.
+
+    Returns:
+        list of lists: A new 2D list representing the transposed matrix.
+    """
+    # Loop through each index i in the range of number of columns
+    # For each i, create a new row by collecting matrix[row][i] from each row
+    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
