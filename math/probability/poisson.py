1594723bd03156e07b098ff141b2cd23b21eb0c8
Relebohile <r.pheko@alustudent>
2025-06-20 15:27:31 -0700

    what the hell

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,29 +1,94 @@
- #!/usr/bin/env python3
-"""
-Poisson distribution class
-"""
+#!/usr/bin/env python3
+""" defines Poisson class that represents Poisson distribution """
+
 
 class Poisson:
-    """Represents a Poisson distribution"""
+    """
+    class that represents Poisson distribution
+
+    class constructor:
+        def __init__(self, data=None, lambtha=1.)
+
+    instance attributes:
+        lambtha [float]: the expected number of occurances in a given time
+
+    instance methods:
+        def pmf(self, k): calculates PMF for given number of successes
+        def cdf(self, k): calculates CDF for given number of successes
+    """
+
     def __init__(self, data=None, lambtha=1.):
-        """Class constructor
+        """
+        class constructor
 
-        Args:
-            data (list): list of the data to estimate the distribution
-            lambtha (float): expected number of occurrences
+        parameters:
+            data [list]: data to be used to estimate the distibution
+            lambtha [float]: the expected number of occurances on a given time
 
-        Raises:
-            TypeError: if data is not a list
-            ValueError: if data has fewer than two elements
-            ValueError: if lambtha is not a positive value
+        Sets the instance attribute lambtha as a float
+        If data is not given:
+            Use the given lambtha or
+            raise ValueError if lambtha is not positive value
+        If data is given:
+            Calculate the lambtha of data
+            Raise TypeError if data is not a list
+            Raise ValueError if data does not contain at least two data points
         """
         if data is None:
-            if lambtha <= 0:
+            if lambtha < 1:
                 raise ValueError("lambtha must be a positive value")
-            self.lambtha = float(lambtha)
+            else:
+                self.lambtha = float(lambtha)
         else:
-            if not isinstance(data, list):
+            if type(data) is not list:
                 raise TypeError("data must be a list")
-            if len(data) < 2:
+            elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
+            else:
+                lambtha = float(sum(data) / len(data))
+                self.lambtha = lambtha
+
+    def pmf(self, k):
+        """
+        calculates the value of the PMF for a given number of successes
+
+        parameters:
+            k [int]: number of successes
+                If k is not an int, convert it to int
+                If k is out of range, return 0
+
+        return:
+            the PMF value for k
+        """
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        e = 2.7182818285
+        lambtha = self.lambtha
+        factorial = 1
+        for i in range(k):
+            factorial *= (i + 1)
+        pmf = ((lambtha ** k) * (e ** -lambtha)) / factorial
+        return pmf
+
+    def cdf(self, k):
+        """
+        calculates the value of the CDF for a given number of successes
+
+        parameters:
+            k [int]: number of successes
+                If k is not an int, convert it to int
+                If k is out of range, return 0
+
+        return:
+            the CDF value for k
+        """
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
