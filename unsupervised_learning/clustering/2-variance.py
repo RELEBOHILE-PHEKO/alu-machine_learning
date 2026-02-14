2d2f4ee94b32798daee1735fa4fa17c06bca3e70
root <r.pheko@alustudent.com>
2026-02-14 13:21:13 -0800

    fix

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -1,41 +1,33 @@
 #!/usr/bin/env python3
-"""This module contains a function that
-calculates total intra-cluster variance for a dataset
+"""
+A function def variance(X, C): that calculates
+the total intra-cluster variance for a data set
 """
 import numpy as np
 
 
 def variance(X, C):
     """
-    Calculates intra-cluster variance for a dataset
+    A function def variance(X, C): that calculates
+    the total intra-cluster variance for a data set
 
-    X: numpy.ndarray (n, d) containing the dataset that
-       will be used for K-means clustering
-        - n: number of data points
-        - d: number of dimensions for each data point
-    C: numpy.ndarray (k, d) containing the centroid
-       for each cluster
-    
+    Args:
+        X is a numpy.ndarray of shape (n, d)
+        containing the data set
+        C is a numpy.ndarray of shape (k, d) containing
+        the centroid means for each cluster
+
     Returns:
-        var: total intra-cluster variance
-        None: on failure
+        - var, or None on failure
+        - var is the total variance
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if (not isinstance(X, np.ndarray) or not isinstance(C, np.ndarray) or
+            len(X.shape) != 2 or len(C.shape) != 2 or
+            X.shape[1] != C.shape[1] or C.shape[1] <= 0 or
+            X.size == 0 or C.size == 0):
         return None
-    if not isinstance(C, np.ndarray) or len(C.shape) != 2:
-        return None
-    if X.shape[1] != C.shape[1]:
-        return None
 
-    # Calculate squared distances from each point to each centroid
-    # Shape: (k, n)
-    squared_distances = np.sum((X - C[:, np.newaxis])**2, axis=2)
-
-    # Find minimum squared distance for each point (to nearest centroid)
-    # Shape: (n,)
-    min_squared_distances = np.min(squared_distances, axis=0)
-
-    # Sum all minimum squared distances
-    var = np.sum(min_squared_distances)
-
+    dist_diff = np.linalg.norm(X - C[:, np.newaxis], axis=2).T
+    minimum_dist = np.min(dist_diff, axis=1)
+    var = np.sum(np.square(minimum_dist))
     return var
