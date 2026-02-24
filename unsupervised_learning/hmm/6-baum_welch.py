a53908bf6a5c21c07080e1e18e7b9c9fdde14572
root <r.pheko@alustudent.com>
2026-02-24 09:39:47 -0800

    yep

--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """Baum-Welch Algorithm for HMM"""
 import numpy as np
-forward = __import__('3-forward').forward
-backward = __import__('5-backward').backward
 
 
 def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
@@ -17,24 +15,44 @@ def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
         return None, None
     if not isinstance(Initial, np.ndarray) or Initial.ndim != 2:
         return None, None
+
     N, M = Emission.shape
     T = Observations.shape[0]
+
     for _ in range(iterations):
-        P, F = forward(Observations, Emission, Transition, Initial)
-        _, B = backward(Observations, Emission, Transition, Initial)
-        if P is None:
-            return None, None
+        # Forward
+        F = np.zeros((N, T))
+        F[:, 0] = Initial[:, 0] * Emission[:, Observations[0]]
+        for t in range(1, T):
+            F[:, t] = np.dot(F[:, t - 1], Transition) * \
+                Emission[:, Observations[t]]
+
+        # Backward
+        B = np.zeros((N, T))
+        B[:, T - 1] = 1
+        for t in range(T - 2, -1, -1):
+            B[:, t] = np.dot(
+                Transition, B[:, t + 1] * Emission[:, Observations[t + 1]])
+
+        # Xi and Gamma
         xi = np.zeros((N, N, T - 1))
         for t in range(T - 1):
-            num = F[:, t].reshape(-1, 1) * Transition * \
-                Emission[:, Observations[t + 1]] * B[:, t + 1]
-            xi[:, :, t] = num / P
+            denom = np.dot(np.dot(F[:, t].T, Transition) *
+                           Emission[:, Observations[t + 1]].T, B[:, t + 1])
+            for i in range(N):
+                xi[i, :, t] = (F[i, t] * Transition[i] *
+                                Emission[:, Observations[t + 1]].T *
+                                B[:, t + 1].T) / denom
+
         gamma = np.sum(xi, axis=1)
-        Transition = np.sum(xi, axis=2) / gamma.sum(axis=1, keepdims=True)
-        gamma_full = np.hstack(
-            (gamma, np.sum(xi[:, :, T - 2], axis=0).reshape(-1, 1)))
-        for k in range(M):
-            Emission[:, k] = np.sum(
-                gamma_full[:, Observations == k], axis=1)
-        Emission /= gamma_full.sum(axis=1, keepdims=True)
+        Transition = np.sum(xi, axis=2) / \
+            np.sum(gamma, axis=1).reshape((-1, 1))
+
+        gamma = np.hstack(
+            (gamma, np.sum(xi[:, :, T - 2], axis=0).reshape((-1, 1))))
+        denom = np.sum(gamma, axis=1)
+        for s in range(M):
+            Emission[:, s] = np.sum(gamma[:, Observations == s], axis=1)
+        Emission = np.divide(Emission, denom.reshape((-1, 1)))
+
     return Transition, Emission
